4a5cc4528c6b22c38297ce66e67f0718faf70aae
gravitional <albertains@163.com>
2020-10-09 01:04:45 +0800

    add tex build.sh

--- latex/build.sh
+++ latex/build.sh
@@ -5,9 +5,9 @@ nameis="name is :"
 # 定义一个打印的函数
 function echo2()
 {
-delimiter1="+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-delimiter2="-------------------------------------------------------"
-echo -e "${delimiter1}\n $* \n${delimiter2}"
+	delimiter1="+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		delimiter2="-------------------------------------------------------"
+		echo -e "${delimiter1}\n $* \n${delimiter2}"
 }
 # 打印当前目录
 echo2 "current directory is " $(pwd)
@@ -22,11 +22,11 @@ echo2 "tex_here $nameis $tex_here"
 # 若有 main.tex，使用之，若没有，则使用 列表中的tex
 result=$(echo $tex_here | grep -iP "main" )
 if [[ $* != "" ]];then
- tex_here=$*
+tex_here=$*
 echo2 "because cml argument given, using <$tex_here> as input"
 elif [[ ${result} != "" ]]
 then
-    tex_here=${tex_usual}
+tex_here=${tex_usual}
 echo2 "because there is a tex file named 'main', so we just compile the, $tex_here"
 else
 echo2 "because there isn't a tex file named 'main', so we just compile the, $tex_here"
@@ -48,9 +48,15 @@ echo2 'echo message'
 ## 之前用 tail 减少输出数量
 ## grep -m 100 -i -n --color -P -B 0 -A 8 "\[\d+\]" "$var.log" | tail -n 50
 # 输出警告
-grep -m 10 -i -n --color -P -B 0 -A 8 "\[\d+\]" "${var}.log"
-# 输出errors
-echo2 'echo errors'
-grep -m 15 -i -n --color -P -B 0 -A 8 "${var}\.tex:\d+:" "${var}.log"
+grep -m 10 -Pi -n --color -B 0 -A 8 "\[\d+\]" "${var}.log"
+# 输出各种形式的错误
+echo2 'echo errors 1'
+grep -Pi -m 15 -n --color -B 1 -A 6 -e "! LaTeX Error:" "${var}.log"
+echo2 'echo errors 2'
+grep -Pi -m 15 -n --color -B 1 -A 6 -e "\? x"       "${var}.log"
+echo2 'echo errors 3'
+grep -Pi -m 15 -n --color -B 0 -A 6 -e "\.tex:\d+:" "${var}.log"
+# 给出错误的具体位置 l.123
+echo2 'the error line postion'
+grep -Pi -m 15 -n --color -B 1 -A 1 "l\.\d+" "${var}.log"
 done
-
