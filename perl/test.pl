dabbc826eda36e50be0006073f8f754c3a34be3a
gravitional <albertains@163.com>
2020-12-23 15:26:17 +0800

    add perl notes

--- perl/test.pl
+++ perl/test.pl
@@ -3,16 +3,4 @@ use strict;
 use warnings;
 #****************************************
  
-sub contextualSubroutine {
-	# Caller wants a list. Return a list
-	return ("Everest", "K2", "Etna", "\n") if wantarray;
-
-	# Caller wants a scalar. Return a scalar
-	return 3 ."\n";
-}
-
-my @array = contextualSubroutine();
-print @array; # "EverestK2Etna"
-
-my $scalar = contextualSubroutine();
-print $scalar; # "3"
+print "what" unless -e "/usr/bin/perl";
